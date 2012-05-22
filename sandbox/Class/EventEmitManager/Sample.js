5316a687c01206be788aeb1e892cdf658ed5a3f9
masato kawakami <mkawakami0304@gmail.com>
2012-05-22 15:05:27 +0900

    コメント追記

--- sandbox/Class/EventEmitManager/Sample.js
+++ sandbox/Class/EventEmitManager/Sample.js
@@ -9,6 +9,10 @@ var sampleB = new SampleB();
 var sampleC = new SampleC();
 var sampleD = new SampleD();
 
+/**
+ * sampleA / sampleB / sampleC の全てのフラグがONになった時、sampleDのdoEmitを実行する
+ * I/FとしてEventEmitManagerは 発火条件methodのcheckEmitを呼び出す。全ての条件がtrueだったとき発火method doEmitを実行する。
+ */
 var eem = new EventEmitManager(sampleD, new Array(sampleA, sampleB, sampleC));
 sampleA.flagOn();
 sampleB.flagOn();
